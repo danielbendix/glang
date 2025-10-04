36a016e1ee8cdbd0be9d678020361cc5b33afc4f
Daniel Bendix <daniel@bendix.io>
2025-10-04 20:05:17 +0200

    Update test suite output.

--- utils/test/suite.py
+++ utils/test/suite.py
@@ -3,9 +3,23 @@ import subprocess
 import tempfile
 import sys
 
-from rich.console import Console
+from rich.console import Console, Group
+from rich.table import Table
+from rich.panel import Panel
+
+from dataclasses import dataclass
 from pathlib import Path
 from expectations import Testcase, parse_file, ParserException
+
+
+@dataclass
+class Failure:
+    header: str
+    lines: list[str]
+
+    def __init__(self, header: str, *lines: str):
+        self.header = header
+        self.lines = [line for line in lines]
 
 
 def validate_test(
@@ -17,19 +31,19 @@ def validate_test(
 
 def run_test(
     glang: Path,
-    opt: str | None,
+    opt: str,
     clang: str,
     input_file: Path,
     testcase: Testcase,
     tmp_dir: Path,
-) -> list[str] | None:
+) -> list[Failure] | None:
     file = Path(input_file)
     name = file.stem
     bitcode = tmp_dir / (name + ".bc")
     bitcode_opt = tmp_dir / (name + "_opt.bc")
     output = tmp_dir / (name + ".out")
 
-    error_lines: list[str] = []
+    errors: list[Failure] = []
 
     glang_result = subprocess.run(
         [glang, file, "-o", bitcode], capture_output=True, text=True
@@ -37,23 +51,29 @@ def run_test(
 
     # TODO: Support compile-failure outcome
     if glang_result.returncode != 0:
-        error_lines.append(
-            f"'{glang}' exited with nonzero return code: {glang_result.returncode}"
+        errors.append(
+            Failure(
+                "Compilation error",
+                f"'{glang}' exited with nonzero return code: {glang_result.returncode}",
+            )
         )
-        return error_lines
+        return errors
 
     if testcase.opt_flags is not None:
         opt_result = subprocess.run(
-            ["opt"] + testcase.opt_flags + [bitcode, "-o", bitcode_opt],
+            [opt] + testcase.opt_flags + [bitcode, "-o", bitcode_opt],
             capture_output=True,
             text=True,
         )
 
         if opt_result.returncode != 0:
-            error_lines.append(
-                f"opt exited with nonzero return code: {opt_result.returncode}"
+            errors.append(
+                Failure(
+                    "Error during LLVM optimization",
+                    f"'{opt}' exited with nonzero return code: {opt_result.returncode}",
+                )
             )
-            return error_lines
+            return errors
     else:
         bitcode_opt = bitcode
 
@@ -62,35 +82,40 @@ def run_test(
     )
 
     if clang_result.returncode != 0:
-        error_lines.append(
-            f"'{clang}' exited with nonzero return code: {clang_result.returncode}"
+        errors.append(
+            Failure(
+                "Linker error",
+                f"'{clang}' exited with nonzero return code: {clang_result.returncode}",
+            )
         )
-        return error_lines
+        return errors
 
     program_result = subprocess.run([output], capture_output=True, text=True)
 
     # TODO: Support execute-failure outcome
     if program_result.returncode != 0:
-        error_lines.append(
-            f"Compiled program failed during execution. Exit code: {program_result.returncode}."
+        errors.append(
+            Failure(
+                "Runtime error",
+                f"Compiled program failed during execution. Exit code: {program_result.returncode}.",
+            )
         )
 
     if testcase.stdout is not None:
         if testcase.stdout.output != program_result.stdout:
-            error_lines.append("Did not get the expected output from stdout.")
-            error_lines.append("Expected: " + repr(testcase.stdout.output))
-            error_lines.append("Got:      " + repr(program_result.stdout))
+            errors.append(
+                Failure(
+                    "stdout expectation failed",
+                    "Expected: " + repr(testcase.stdout.output),
+                    "Got:      " + repr(program_result.stdout),
+                )
+            )
 
-    return error_lines
+    return errors
 
 
 app = typer.Typer()
 console = Console()
-
-
-@app.command(help="Validate test suite")
-def validate(directory: Path):
-    pass
 
 
 def load_testcase(code_file: Path, testcase_directory: Path) -> Testcase | None:
@@ -123,6 +148,30 @@ def load_testcases(code_files: list[Path], testcase_directory: Path) -> list[Tes
     return testcases
 
 
+@app.command(help="Validate test suite")
+def validate(directory: Path):
+    testcase_directory = directory / "expected"
+
+    code_files = list(directory.glob("*.gg"))
+    code_files.sort()
+
+    if len(code_files) == 0:
+        console.print("No code files in test suite.", style="bold red")
+        return
+
+    testcases = load_testcases(code_files, testcase_directory)
+
+    table = Table(title="Test cases", show_header=False)
+    table.add_column()
+
+    for code_file, testcase in zip(code_files, testcases):
+        if testcase is None:
+            return
+        name = testcase.name if testcase.name is not None else code_file.stem
+        table.add_row(name)
+    console.print(table)
+
+
 @app.command(help="Run test suite")
 def run(directory: Path, glang: Path, clang: str, opt: str):
     testcase_directory = directory / "expected"
@@ -138,6 +187,10 @@ def run(directory: Path, glang: Path, clang: str, opt: str):
 
     temporary_directory = tempfile.TemporaryDirectory()
 
+    table = Table(title="Test suite result")
+    table.add_column("Test case", style="white")
+    table.add_column("Result", style="white")
+
     for code_file, testcase in zip(code_files, testcases):
         if testcase is None:
             return
@@ -147,11 +200,19 @@ def run(directory: Path, glang: Path, clang: str, opt: str):
         )
 
         if not errors:
-            console.print(f"Test case '{name}' succeeded", style="green")
+            table.add_row(name, "Succeeded", style="green")
         else:
-            console.print(f"Test case '{name}' failed:", style="bold red")
+            num_errors = len(errors)
+            error_noun = "error" if num_errors == 1 else "errors"
+            panels: list[Panel | str] = [f"Failed - {num_errors} {error_noun}"]
             for line in errors:
-                console.print(line, style="red")
+                panels.append(
+                    Panel("\n".join(line.lines), title=line.header, border_style="red", style="white")
+                )
+            group = Group(*panels)
+            table.add_row(name, group, style="red")
+
+    console.print(table)
 
 
 if __name__ == "__main__":
